553ec46ae2e6f6abad120c05cef0a905b3585798
genaver <genaver@gmail.com>
2014-01-23 17:10:37 +0300

    Initial commit

---
+++ AOUT.Logan/LogAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOUT.Logan
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".sln"))
+                return false;
+
+            return true;
+        }
+    }
+}
